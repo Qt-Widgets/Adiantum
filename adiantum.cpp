344e04504506c2703d347ce473a1eda6d21b73e7
Screwdr1ver <screwdr1ver@users.noreply.github.com>
2018-08-23 20:26:54 +0300

    Changed window style to undecorated

--- adiantum.cpp
+++ adiantum.cpp
@@ -1,7 +1,8 @@
 #include "adiantum.h"
 
 Adiantum::Adiantum(QWidget *parent) : QMainWindow(parent) {
-
+    this->setStyleSheet("background:black;");
+    this->setWindowFlags(Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint);
 }
 
 Adiantum::~Adiantum() {}
